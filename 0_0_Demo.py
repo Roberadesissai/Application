a26c1c873d9b6bb9119e50b0d1b3f91f9bf70a34
CodeByRobera <101603054+Roberadesissai@users.noreply.github.com>
2023-11-06 20:48:17 -0500

    update

--- 0_0_Demo.py
+++ 0_0_Demo.py
@@ -1,27 +1,37 @@
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as root
 
 # Window
-window = tk.Tk()
+window = root.Window()
 window.title("Demo")
 window.geometry("300x150")
 
 # Label
-label = ttk.Label(master = window, text="Miles to Kilometers Converter", font = ("Calibri", 15, "bold"))
+label = root.Label(master = window, text="Miles to Kilometers Converter", font = ("Calibri", 15, "bold"))
 label.pack()
+
+def convert():
+    miles = entry_into.get()
+    kilometers = miles * 1.609
+    output_string.set(f"{kilometers:.2f} km")
 
 
 # input field
 frame = tk.Frame(master = window)
-entry = ttk.Entry(master = frame)
+entry_into = tk.IntVar()
+entry = root.Entry(master = frame, textvariable = entry_into)
 
 # button
-button = ttk.Button(master = frame, text = "Convert")
-entry.pack()
-button.pack()
-frame.pack()
+button = root.Button(master = frame, text = "Convert", command = convert )
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+frame.pack(pady = 10)
 
 
+# output field
+output_string = tk.StringVar()
+output_label = root.Label(master = window, text = "output", textvariable = output_string) 
+output_label.pack(pady = 5)
 
 
 
